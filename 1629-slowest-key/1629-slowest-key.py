b5944502567cdbb9ad5fc076df76c5e729cc67ea
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-02-25 13:23:49 -0500

    Time: 60 ms (81.96%), Space: 14.2 MB (75.69%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.py
+++ 1629-slowest-key/1629-slowest-key.py
@@ -1,13 +1,4 @@
-import numpy as np
-class Solution:
-    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        times = []
-        times.append(releaseTimes[0])
-        [times.append(releaseTimes[i] - releaseTimes[i-1]) for i in range(1,len(releaseTimes))]
-        m = max(times)
-        a = np.array(times)
-        r = np.where(a==m)
-        ans = np.array(list(keysPressed))
-        ans = ans[r]
-        ans = np.sort(ans)
-        return ans[-1]
+class Solution(object):
+    def slowestKey(self, r, k):
+        return max(zip(map(sub, r, [0, *r]), k))[1]
+        
