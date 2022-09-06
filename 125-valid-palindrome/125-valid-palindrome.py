422d8674eb1f2abee1a01b9dde8a0c997d057b78
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-09-06 10:03:56 -0400

    Time: 96 ms (24.95%), Space: 14.4 MB (85.74%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,12 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        ans = []
-        s = s.lower()
-        for char in s:
-            if (0 <= ord(char) - ord('a') < 26) or (ord('0') <= ord(char) <= ord('9')):
-                ans.append(char)
-        for i in range(len(ans)):
-            if ans[i] != ans[len(ans) - i - 1]:
+        l, r = 0, len(s) -1
+        while l < r:
+            if not ((0 <= ord(s[l]) - ord('a') < 26) or (0 <= ord(s[l]) - ord('A') < 26) or (ord('0') <= ord(s[l]) <= ord('9'))):
+                l += 1
+                continue
+            if not ((0 <= ord(s[r]) - ord('a') < 26) or (0 <= ord(s[r]) - ord('A') < 26) or (ord('0') <= ord(s[r]) <= ord('9'))):
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
                 return False
+            l += 1
+            r -= 1
         return True
         
