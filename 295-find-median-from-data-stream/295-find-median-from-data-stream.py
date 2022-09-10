2e56c7255f96d42bc5d32fe0869df94455375276
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-09-10 18:27:11 -0400

    Time: 1536 ms (16.47%), Space: 35.4 MB (97.93%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -2,18 +2,35 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.heap = []
+        self.small, self.large = [], []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.heap, num)
+        
+        heapq.heappush(self.small, -num)
+        
+        
+            
+        # make sure order is satisfied
+        if self.small and self.large and -self.small[0] > self.large[0]:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+            if len(self.large) > len(self.small) + 1:
+                heapq.heappush(self.small, -heapq.heappop(self.large))
+            
+        # make sure len is approx equal
+        
+        if len(self.small) > len(self.large) + 1:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+
+        
 
     def findMedian(self) -> float:
-        n = len(self.heap)
-        self.heap.sort()
-        if n % 2:
-            return self.heap[n//2]
+        s, l = len(self.small), len(self.large)
+        if (s+l) % 2:
+            if s > l:
+                return -self.small[0]
+            return self.large[0]
         
-        return (self.heap[n//2] + self.heap[(n//2) - 1]) / 2
+        return (self.large[0] - self.small[0])/2
         
 
 
