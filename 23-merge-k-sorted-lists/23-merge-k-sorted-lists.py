b3acf6e9740f4aa4628bff346652ed518cfde185
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-09-10 15:44:34 -0400

    Time: 107 ms (93.83%), Space: 17.6 MB (74.09%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -3,24 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from heapq import merge
-
-def travel(node): # Convert linked list to list
-    root = node
-    while root:
-        yield root.val
-        root = root.next
-
-def getLinkedList(node): # Convert list to linked list
-    temp = ListNode(None)
-    head = temp
-    for i in range(len(node)):
-        temp.next = ListNode(node[i])
-        temp = temp.next
-    return head.next
-
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        iterables = [list(travel(x)) for x in lists]
-        ans = list(merge(*iterables))
-        return getLinkedList(ans)
+        def merge(list1, list2):
+            if not list1:
+                return list2
+            if not list2:
+                return list1
+            prev = ListNode(None)
+            head = prev
+            curr1 = list1
+            curr2 = list2
+            while curr1 and curr2:
+                if curr1.val < curr2.val:
+                    prev.next, curr1 = curr1, curr1.next
+                else:
+                    prev.next, curr2 = curr2, curr2.next
+                prev = prev.next
+            prev.next = curr1 if curr1 else curr2
+            return head.next
+        
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                merged.append(merge(l1, l2))
+            lists = merged
+        return lists[0] if lists else None
