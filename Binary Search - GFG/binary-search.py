8200427062d7b3cdd6ec631eb4b606f8373e11a6
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-12-17 17:45:24 -0500

    Added solution - LeetHub

---
+++ Binary Search - GFG/binary-search.py
@@ -0,0 +1,33 @@
+#User function template for Python
+
+class Solution:	
+	def binarysearch(self, arr, n, k):
+		# code here
+		l,r = 0, n-1
+		m = 0
+		while l <= r:
+		    m = (l+r) // 2
+		    if arr[m] == k:
+		        return m
+		    if arr[m] < k:
+		        l = m+1
+		    else:
+		        r = m-1
+        return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+if __name__ == '__main__':
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int, input().strip().split(' ')))
+        k=int(input())
+        ob = Solution()
+        print (ob.binarysearch(arr, n, k))
+
+
+# } Driver Code Ends
