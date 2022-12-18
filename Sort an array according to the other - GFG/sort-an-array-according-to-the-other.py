58e004fbc82cb60a923475a9bb62b48663ee3413
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-12-18 13:59:49 -0500

    Added solution - LeetHub

---
+++ Sort an array according to the other - GFG/sort-an-array-according-to-the-other.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    # A1[] : the input array-1
+    # N : size of the array A1[]
+    # A2[] : the input array-2
+    # M : size of the array A2[]
+    
+    #Function to sort an array according to the other array.
+    def relativeSort (self,A1, N, A2, M):
+        # Your Code Here
+        hm = {A2[i]:i for i in range(len(A2))}
+        
+        
+        A1.sort(key=lambda x: hm[x] if x in hm else M+x)
+        
+        return A1
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int (input ())
+    while t > 0:
+        n, m = list(map(int, input().split()))
+        a1 = list(map(int, input().split()))
+        a2 = list(map(int, input().split()))
+        
+        ob=Solution()
+        a1 = ob.relativeSort (a1, n, a2, m)
+        print (*a1, end = " ")
+        
+        print ()
+        
+        t -= 1
+
+# } Driver Code Ends
