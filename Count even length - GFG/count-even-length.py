4f8f87a4114ff1a245f02d808381f3fd4bd3c7cc
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-12-31 15:39:47 -0500

    Added solution - LeetHub

---
+++ Count even length - GFG/count-even-length.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+import operator as op
+from functools import reduce
+
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n-r, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer // denom  # or / in Python 2
+    
+class Solution:
+	def compute_value(self, n):
+		# Code here
+        return ncr(2*n,n) % (1000000007)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		ob = Solution()
+		ans = ob.compute_value(n)
+		print(ans)
+# } Driver Code Ends
